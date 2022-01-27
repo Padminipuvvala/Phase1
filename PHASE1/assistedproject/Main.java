fd08141fc9590f119a0ceedc157e0941c21f10d6
Padminipuvvala <97678273+Padminipuvvala@users.noreply.github.com>
2022-01-27 17:58:05 +0530

    Add files via upload

--- PHASE1/assistedproject/Main.java
+++ PHASE1/assistedproject/Main.java
@@ -1,23 +1,21 @@
-package assistedproject;
-class MyException extends Exception 
-{ 
-    public MyException(String s) 
-    { 
-        super(s); 
-    } 
-} 
-public class Main 
-{ 
-    public static void main(String args[]) 
-    { 
-        try
-        { 
-            throw new MyException("temp"); 
-        } 
-        catch (MyException ex) 
-        { 
-            System.out.println("Caught"); 
-            System.out.println(ex.getMessage()); 
-        } 
-    } 
+package project1;
+
+class Main {
+  public static void main(String[] args) {
+
+    // creates objects of wrapper class
+    Integer obj1 = Integer.valueOf(23);
+    Double obj2 = Double.valueOf(5.55);
+    Boolean obj3 = Boolean.valueOf(true);
+
+    // converts into primitive types
+    int var1 = obj1.intValue();
+    double var2 = obj2.doubleValue();
+    boolean var3 = obj3.booleanValue();
+
+    // print the primitive values
+    System.out.println("The value of int variable: " + var1);
+    System.out.println("The value of double variable: " + var2);
+    System.out.println("The value of boolean variable: " + var3);
+  }
 }
